3de6dec623dd8d1c75c61d4b58010072e429a964
Ram Kinkar Singh <ramkinkar2512@gmail.com>
2022-02-17 11:25:52 +0530

    Update transaction_list.dart
    Add ternary expression to replace empty transaction list with gif and arrow stimulating user to add transaction.

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -8,52 +8,78 @@ class TransactionList extends StatelessWidget {
   TransactionList(this.transactions);
 
   Widget build(BuildContext context) {
-    return Container(
-      height: 400,
-      child: ListView.builder(
-        itemBuilder: (context, index) {
-          return Card(
-            elevation: 40,
-            child: Row(
-              children: [
-                Container(
-                  height: 80,
-                  width: 200,
-                  margin: EdgeInsets.all(15),
-                  decoration: BoxDecoration(
-                      border: Border.all(
-                    width: 5,
-                    color: Theme.of(context).primaryColor,
-                  )),
-                  padding: EdgeInsets.all(15),
-                  child: Text(
-                    '₹ ${transactions[index].amount.toStringAsFixed(2)}',
-                    style: TextStyle(
-                      fontSize: 30,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
+    return transactions.isEmpty
+        ? Column(
+            children: [
+              SizedBox(
+                height: 10,
+              ),
+              Text(
+                'No transactions added yet.',
+                style: Theme.of(context).textTheme.headline5,
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Container(
+                height: 200,
+                width: 450,
+                child: Image.asset(
+                  'assets/images/gif2.gif',
+                  fit: BoxFit.fill,
                 ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      transactions[index].tittle,
-                      style:
-                          TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
-                    ),
-                    Text(
-                      DateFormat.yMMMd().format(transactions[index].date),
-                      style: TextStyle(fontSize: 15),
-                    )
-                  ],
-                )
-              ],
+              ),
+              Text('Add transaction here',
+                  style: Theme.of(context).textTheme.headline4),
+              Container(
+                height: 200,
+                child: Image.asset(
+                  'assets/images/image2.jpg',
+                  fit: BoxFit.cover,
+                ),
+              )
+            ],
+          )
+        : Container(
+            height: 400,
+            child: ListView.builder(
+              itemBuilder: (context, index) {
+                return Card(
+                  elevation: 40,
+                  child: Row(
+                    children: [
+                      Container(
+                        height: 80,
+                        width: 200,
+                        margin: EdgeInsets.all(15),
+                        decoration: BoxDecoration(
+                            border: Border.all(
+                          width: 5,
+                          color: Theme.of(context).primaryColor,
+                        )),
+                        padding: EdgeInsets.all(15),
+                        child: Text(
+                          '₹ ${transactions[index].amount.toStringAsFixed(2)}',
+                          style: Theme.of(context).textTheme.headline5,
+                        ),
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(transactions[index].tittle,
+                              style: Theme.of(context).textTheme.headline4),
+                          Text(
+                            DateFormat.yMMMd().format(transactions[index].date),
+                            style: Theme.of(context).textTheme.subtitle1,
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                );
+              },
+              itemCount: transactions.length,
             ),
           );
-        },
-        itemCount: transactions.length,
-      ),
-    );
   }
 }
